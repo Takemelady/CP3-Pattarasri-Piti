ee9c4eb71c407178a57a35a59559c071321a8c9c
Yotsapon Saenghong <s.yotsapon@gmail.com>
2021-07-04 22:57:32 +0700

    Add exercise4

---
+++ Exercise4_Pattrasri_P.py
@@ -0,0 +1,10 @@
+foundeng = 69
+genbiz = 70
+introcs = 56
+comprogram = 69
+
+print("--- Your Score ---")
+print("Foundation English", ":", foundeng)
+print("General Business",   ":", genbiz)
+print("Introduction to Computer Systems", ":", introcs)
+print("Computer Programming", ":", comprogram)

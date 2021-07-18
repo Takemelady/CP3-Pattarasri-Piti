6a57dbd84f5ce5a4941782ee6e0ea557844de44c
Yotsapon Saenghong <s.yotsapon@gmail.com>
2021-07-18 16:49:56 +0700

    Add exercise5

---
+++ Exercise5_1_Pattarasri_P.py
@@ -0,0 +1,10 @@
+a = 10
+b = 2
+a += b
+print(a)
+a -= b
+print(a)
+a *= b
+print(a)
+a /= b
+print(a)
---
+++ Exercise5_2_Pattarasri_P.py
@@ -0,0 +1,4 @@
+distanceInKM = int(input("Distance in Kilometers:"))
+durationTime = int(input("Time Duration in Hours:"))
+velocity = distanceInKM / durationTime
+print(velocity, "km/hr")
